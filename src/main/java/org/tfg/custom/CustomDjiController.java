72719dc9bfa4d8a7a4003842bce74e316fd63027
idvimosUPM <id.vimos@alumnos.upm.es>
2025-03-03 21:33:30 +0100

    feat: recognition objects added to CustomDjiController

--- src/main/java/org/tfg/custom/CustomDjiController.java
+++ src/main/java/org/tfg/custom/CustomDjiController.java
@@ -1,6 +1,7 @@
 package org.tfg.custom;
 
 import com.cyberbotics.webots.controller.Camera;
+import com.cyberbotics.webots.controller.CameraRecognitionObject;
 import com.cyberbotics.webots.controller.Compass;
 import com.cyberbotics.webots.controller.GPS;
 import com.cyberbotics.webots.controller.Gyro;
@@ -31,6 +32,7 @@ public class CustomDjiController extends CustomRobot {
     public CustomDjiController() {
         camera = getCamera("camera");
         camera.enable(timeStep);
+        camera.recognitionEnable(timeStep);
         frontLeftLed = getLED("front left led");
         frontRightLed = getLED("front right led");
         imu = getInertialUnit("inertial unit");
@@ -80,6 +82,30 @@ public class CustomDjiController extends CustomRobot {
 
             cameraRollMotor.setPosition(-0.115 * rollVelocity);
             cameraPitchMotor.setPosition(-0.1 * pitchVelocity);
+
+            int numberOfObjects = camera.getRecognitionNumberOfObjects();
+
+            CameraRecognitionObject[] objects = camera.getRecognitionObjects();
+            for (int i = 0; i < numberOfObjects; i++) {
+                System.out.println("Model of object " + i + ": " + objects[i].getModel());
+                System.out.println("Id of object " + i + ": " + objects[i].getId());
+                System.out.println("Relative position of object " + i + ": " + objects[i].getPosition()[0] + " " +
+                        objects[i].getPosition()[1] + " " + objects[i].getPosition()[2]);
+                System.out.println("Relative orientation of object " + i + ": " + objects[i].getOrientation()[0] + " " +
+                        objects[i].getOrientation()[1] + " " + objects[i].getOrientation()[2] + " " + objects[i].getOrientation()[3]);
+                System.out.println("Size of object " + i + ": " + objects[i].getSize()[0] + " " + objects[i].getSize()[1]);
+                System.out.println("Position of the object " + i + " on the camera image: " + objects[i].getPositionOnImage()[0] + " " +
+                        objects[i].getPositionOnImage()[1]);
+                System.out.println("Size of the object " + i + " on the camera image: " + objects[i].getSizeOnImage()[0] + " " +
+                        objects[i].getSizeOnImage()[1]);
+                for (int j = 0; j < objects[i].getNumberOfColors(); j++) {
+                    int colorIndex = 3 * j;
+                    if (colorIndex + 2 < objects[i].getColors().length) {
+                        System.out.println("- Color " + (j + 1) + "/" + objects[i].getNumberOfColors() + ": " +
+                                objects[i].getColors()[colorIndex] + " " + objects[i].getColors()[colorIndex + 1] + " " + objects[i].getColors()[colorIndex + 2]);
+                    }
+                }
+            }
 
             double rollDisturbance = 0.0;
             double pitchDisturbance = 0.0;
