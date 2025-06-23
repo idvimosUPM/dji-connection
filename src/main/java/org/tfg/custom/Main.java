14cfca6e0b72ef6551adafeb8010fac039ef5b1c
idvimosUPM <id.vimos@alumnos.upm.es>
2025-06-23 14:40:50 +0200

    feat: Main modified to finish webots when finish the simulation.

--- src/main/java/org/tfg/custom/Main.java
+++ src/main/java/org/tfg/custom/Main.java
@@ -12,6 +12,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
+
+    private static Process webotsProcess;
+
     public static void main(String[] args) throws IOException {
 
         // Start Webots
@@ -20,7 +23,7 @@ public class Main {
         CustomDjiController controller = new CustomDjiController();
 
         // Instructions file uploaded
-        String code = new String(Files.readAllBytes(Paths.get("/Users/TFG/Documents/TFG/backend/dji-connection/src/main/resources/instructions/instructions.txt")));
+        String code = new String(Files.readAllBytes(Paths.get("/Users/TFG/Documents/TFG/backend/dji-connection/src/main/resources/instructions/instructions-2.txt")));
 
         // Lexer and Parser
         CustomDjiControllerLexer lexer = new CustomDjiControllerLexer(CharStreams.fromString(code));
@@ -34,14 +37,30 @@ public class Main {
         ParseTreeWalker walker = new ParseTreeWalker();
         CustomDjiControllerListener listener = new CustomDjiControllerListener(controller);
         walker.walk(listener, tree);
+
+        // Stop Webots
+        stopWebots();
+
+        System.out.println("SIMULATION FINISHED");
     }
 
     private static void startWebots(String worldFilePath) {
         try {
             ProcessBuilder processBuilder = new ProcessBuilder("/Applications/Webots.app/Contents/MacOS/webots", worldFilePath);
-            processBuilder.start();
+            webotsProcess = processBuilder.start();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void stopWebots() {
+        if (webotsProcess != null) {
+            try {
+                Thread.sleep(3000); // wait 3 seconds before stop
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            webotsProcess.destroy();
         }
     }
 }
